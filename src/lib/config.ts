a23fc1c7ca7e5be48ffb50a59f01bc948a7b4558
Kristián Kunc <kristian.kunc@gmail.com>
2023-05-02 14:54:22 +0200

    fuck

--- src/lib/config.ts
+++
@@ -1,9 +0,0 @@
-export const masterPassword = "milujuvokovice_07";
-
-export const mysqlConfig = {
-	host: "192.168.0.104",
-	port: 3306,
-	username: "basnicky",
-	password: "^p7L37*!$^8t",
-	database: "basnicky"
-};
