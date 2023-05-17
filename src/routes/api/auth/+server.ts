4e9edcff1be7b47b1194b9aaa903962f5b2f5f0e
Kristián Kunc <kristian.kunc@gmail.com>
2023-05-17 16:25:53 +0200

    add auth ratelimit

--- src/routes/api/auth/+server.ts
+++ src/routes/api/auth/+server.ts
@@ -1,10 +1,27 @@
-import { type RequestHandler } from "@sveltejs/kit";
+import type { RequestHandler } from "@sveltejs/kit";
 import { masterPassword } from "$lib/config";
 
+let requestCache = new Map<string, number>();
+
 export const POST = (async ({ request }) => {
+	// get the user ip
+	let ip = request.headers.get("CF-Connecting-IP") || request.headers.get("X-Forwarded-For") || request.headers.get("X-Real-IP") || "0";
+
+	requestCache.set(ip, (requestCache.get(ip) || 0) + 1);
+	let retries = requestCache.get(ip) || 0;
+
+	if (retries > 5) {
+		setTimeout(() => requestCache.delete(ip), 60000);
+
+		return new Response(null, {
+			status: 429,
+		});
+	}
+
 	let password = request.headers.get("Authorization");
 
 	if (password == masterPassword) {
+		requestCache.delete(ip);
 		return new Response(null, {
 			status: 200,
 			headers: {
